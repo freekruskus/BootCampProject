99af64e0c0baf0b8d136fc40f6a15ad3733565b7
Freek Ruskus <freek.ruskus@polteq.com>
2018-07-26 14:15:59 +0200

    6.01 and 6.02

---
+++ src/test/java/chapterSix/SignOutTest.java
@@ -0,0 +1,41 @@
+package chapterSix;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SignOutTest {
+
+    @Test
+    public void SignIn(){
+        ChromeDriverManager.getInstance().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://techblog.polteq.com/testshop/index.php");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.className("login")).click();
+        driver.findElement(By.id("email")).sendKeys("freek.ruskus@polteq.com");
+        driver.findElement(By.id("passwd")).sendKeys("bootcamptest");
+        driver.findElement(By.id("SubmitLogin")).click();
+
+        WebElement HeaderValue = driver.findElement(By.className("page-heading"));
+        WebElement AccountName = driver.findElement(By.className("account"));
+        boolean SignOutPresent = driver.findElements(By.className("logout")).size() > 0;
+
+        Assert.assertEquals(HeaderValue.getText(),"MY ACCOUNT");
+        Assert.assertEquals(AccountName.getText(), "Freek Ruskus");
+        Assert.assertTrue(SignOutPresent);
+
+        driver.findElement(By.className("logout")).click();
+
+        boolean SignInPresent = driver.findElements(By.className("login")).size() > 0;
+        Assert.assertTrue(SignInPresent);
+
+        driver.quit();
+    }
+}
